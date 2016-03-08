9fe5720121b2f9f1c48fe3505cb475e1ba0cf518
venujan99 <venujan99@github.com>
2016-03-08 14:45:22 -0500

    loading file name to table

--- spamDetector_SV/src/spamDetector/DataSource.java
+++ spamDetector_SV/src/spamDetector/DataSource.java
@@ -3,20 +3,24 @@ package spamDetector;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.ArrayList;
+
 /**
  * Created by venujan on 06/03/16.
  */
 public class DataSource {
 
-    public static ObservableList<TestFile> getAllSpam() {
+    private ArrayList<String> words;
+
+   public static ObservableList<TestFile> getAllSpam() {
         ObservableList<TestFile> spamMessages = FXCollections.observableArrayList();
 
-        spamMessages.add(new TestFile("100100100", "Janet", 2.85));
-        spamMessages.add(new TestFile("100100101", "Abichal", 2.90));
-        spamMessages.add(new TestFile("100100102", "Cecile", 3.4));
-        spamMessages.add(new TestFile("100100103", "Pablo", 4.0));
-        spamMessages.add(new TestFile("100100104", "Flora", 4.3));
 
+        spamMessages.add(new TestFile(Main.fList.toString(), "Ham", 2.85));
+        spamMessages.add(new TestFile("100100101", "Spam", 2.90));
+        spamMessages.add(new TestFile("100100102", "Spam", 3.4));
+        spamMessages.add(new TestFile("100100103", "Ham", 4.0));
+        spamMessages.add(new TestFile("100100104", "Spam", 4.3));
 
         return spamMessages;
     }
--- spamDetector_SV/src/spamDetector/Main.java
+++ spamDetector_SV/src/spamDetector/Main.java
@@ -23,8 +23,11 @@ public class Main extends Application {
     private Stage window;
     private BorderPane layout;
     private TableView<TestFile> table;
+    public static File[] fList;
+    public static List list;
 
-    @Override
+
+
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Spam Detector");
 
@@ -32,95 +35,93 @@ public class Main extends Application {
         directoryChooser.setInitialDirectory(new File("."));
         File mainDirectory = directoryChooser.showDialog(primaryStage);
 
+        File directory = new File(String.valueOf(mainDirectory));
 
-       File directory = new File(String.valueOf(mainDirectory));
+        //get all the files from a directory and putting into a list
+        fList = directory.listFiles();
 
-        //get all the files from a directory
-
-        File[] fList = directory.listFiles();
 
         for (File file : fList) {
 
             if (file.isFile()) {
 
-               // System.out.println(file.getName());
+                // System.out.println(file.getName());
                 List<String> list = new ArrayList<String>();
                 list.add(file.getName());
                 System.out.println(list);
 
-                    // System.out.println(mainDirectory);
-
-                }
             }
-
-
+        }
 
         /* create the table (for the center of the user interface) */
-            table = new TableView<>();
-            table.setItems(DataSource.getAllSpam());
-            table.setEditable(true);
+        table = new TableView<>();
+        table.setItems(DataSource.getAllSpam());
+        table.setEditable(true);
 
 
-            TableColumn<TestFile, String> fileNameColumn = null;
-            fileNameColumn = new TableColumn<>("File");
-            fileNameColumn.setMinWidth(300);
-            fileNameColumn.setCellValueFactory(new PropertyValueFactory<>("fileName"));
-            fileNameColumn.setOnEditCommit((TableColumn.CellEditEvent<TestFile, String> event) -> {
-                ((TestFile) event.getTableView().getItems().get(event.getTablePosition().getRow())).setFileName(event.getNewValue());
-            });
+        TableColumn<TestFile, String> fileNameColumn = null;
+        fileNameColumn = new TableColumn<>("File");
+        fileNameColumn.setMinWidth(300);
+        fileNameColumn.setCellValueFactory(new PropertyValueFactory<>("fileName"));
+        fileNameColumn.setOnEditCommit((TableColumn.CellEditEvent<TestFile, String> event) -> {
+            ((TestFile) event.getTableView().getItems().get(event.getTablePosition().getRow())).setFileName(event.getNewValue());
+        });
 
-            TableColumn<TestFile, String> actualClassColumn = null;
-            actualClassColumn = new TableColumn<>("Actual Class");
-            actualClassColumn.setMinWidth(150);
-            actualClassColumn.setCellValueFactory(new PropertyValueFactory<>("actualClass"));
-            actualClassColumn.setCellFactory(TextFieldTableCell.<TestFile>forTableColumn());
-            actualClassColumn.setOnEditCommit((TableColumn.CellEditEvent<TestFile, String> event) -> {
-                ((TestFile) event.getTableView().getItems().get(event.getTablePosition().getRow())).setActualClass(event.getNewValue());
-            });
+        TableColumn<TestFile, String> actualClassColumn = null;
+        actualClassColumn = new TableColumn<>("Actual Class");
+        actualClassColumn.setMinWidth(150);
+        actualClassColumn.setCellValueFactory(new PropertyValueFactory<>("actualClass"));
+        actualClassColumn.setCellFactory(TextFieldTableCell.<TestFile>forTableColumn());
+        actualClassColumn.setOnEditCommit((TableColumn.CellEditEvent<TestFile, String> event) -> {
+            ((TestFile) event.getTableView().getItems().get(event.getTablePosition().getRow())).setActualClass(event.getNewValue());
+        });
 
-            TableColumn<TestFile, Double> spamProbabilityColumn = null;
-            spamProbabilityColumn = new TableColumn<>("Spam Probability");
-            spamProbabilityColumn.setMinWidth(300);
-            spamProbabilityColumn.setCellValueFactory(new PropertyValueFactory<>("spamProbability"));
+        TableColumn<TestFile, Double> spamProbabilityColumn = null;
+        spamProbabilityColumn = new TableColumn<>("Spam Probability");
+        spamProbabilityColumn.setMinWidth(300);
+        spamProbabilityColumn.setCellValueFactory(new PropertyValueFactory<>("spamProbability"));
 
-            table.getColumns().add(fileNameColumn);
-            table.getColumns().add(actualClassColumn);
-            table.getColumns().add(spamProbabilityColumn);
+        table.getColumns().add(fileNameColumn);
+        table.getColumns().add(actualClassColumn);
+        table.getColumns().add(spamProbabilityColumn);
 
         /* create an edit form (for the bottom of the user interface) */
-            GridPane editArea = new GridPane();
-            editArea.setPadding(new Insets(10, 10, 10, 10));
-            editArea.setVgap(10);
-            editArea.setHgap(10);
+        GridPane editArea = new GridPane();
+        editArea.setPadding(new Insets(10, 10, 10, 10));
+        editArea.setVgap(10);
+        editArea.setHgap(10);
 
-            Label accuracyLabel = new Label("Accuracy:");
-            editArea.add(accuracyLabel, 0, 0);
-            TextField accField = new TextField();
-            accField.setPromptText("");
-            editArea.add(accField, 1, 0);
+        Label accuracyLabel = new Label("Accuracy:");
+        editArea.add(accuracyLabel, 0, 0);
+        TextField accField = new TextField();
+        accField.setPromptText("");
+        editArea.add(accField, 1, 0);
 
-            Label precisionLabel = new Label("Precision:");
-            editArea.add(precisionLabel, 0, 1);
-            TextField precisionField = new TextField();
-            precisionField.setPromptText("");
-            editArea.add(precisionField, 1, 1);
+        Label precisionLabel = new Label("Precision:");
+        editArea.add(precisionLabel, 0, 1);
+        TextField precisionField = new TextField();
+        precisionField.setPromptText("");
+        editArea.add(precisionField, 1, 1);
 
 
         /* arrange all components in the main user interface */
-            layout = new BorderPane();
-            // layout.setTop(menuBar);
-            layout.setCenter(table);
-            layout.setBottom(editArea);
+        layout = new BorderPane();
+        // layout.setTop(menuBar);
+        layout.setCenter(table);
+        layout.setBottom(editArea);
 
-            Scene scene = new Scene(layout, 800, 600);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        }
-    
+        Scene scene = new Scene(layout, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
+
+
     }
 
 
 }
+
+
